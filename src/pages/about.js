43c84932e0f604554d6d90699098facfb02a37a1
svensone <svenson.ertel@gmail.com>
2019-03-09 13:49:15 +0800

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const AboutPage = () =>(
+    <div>
+      <h1>About Us</h1>
+      <p>This is my first Gatsby Site - learning for portfolio page.</p>
+    </div>
+  )
+
+
+export default AboutPage
+ 
---
+++ src/pages/service.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const Services = () => 
+   (
+    <div>
+      <h1>Service</h1>
+      <p>my services .</p>
+    </div>
+  )
+
+
+export default Services
+ 
